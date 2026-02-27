5d9837a8c1960bb82c36bd9a42755c8bb5e9c3a1
武田隼人 <hayatorani63@takedahayatonoMacBook-Air.local>
2026-02-28 00:08:50 +0900

    Configure real Firebase credentials for data sync

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,13 @@ import { getFirestore } from "firebase/firestore";
 
 // TODO: Replace with real config from user
 const firebaseConfig = {
-    apiKey: "PLACEHOLDER",
-    authDomain: "fmmk-bussinessgoal.firebaseapp.com",
-    projectId: "fmmk-bussinessgoal",
-    storageBucket: "fmmk-bussinessgoal.firebasestorage.app",
-    messagingSenderId: "PLACEHOLDER",
-    appId: "PLACEHOLDER"
+    apiKey: "AIzaSyAveOH8wL6BcNkredO35-scOP270MLEYMs",
+    authDomain: "sucess-59ee9.firebaseapp.com",
+    projectId: "sucess-59ee9",
+    storageBucket: "sucess-59ee9.firebasestorage.app",
+    messagingSenderId: "661662291977",
+    appId: "1:661662291977:web:8ba19cfb5f6fc4643d80d8",
+    measurementId: "G-043TKVEPPB"
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
